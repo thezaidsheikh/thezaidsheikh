a4d2b144e250edbaf60ed1658c53f0fdac039c17
Zaid Qureshi <zaid.qureshi@lmsin.com>
2023-06-17 19:39:38 +0530

    removed unwanted code and terms

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
       'Zaid Qureshi is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://thezaidsheikh.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@thezaidsheikh',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
